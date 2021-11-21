351a93eae933fac547b753444d382888ea49f92b
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-11-21 18:21:37 -0100

    minor improvements

--- analysis.py
+++ analysis.py
@@ -24,40 +24,46 @@ def question2():
     answerNoise = 0.01
     return answerDiscount, answerNoise
 
+
 def question3a():
-    answerDiscount = .1
-    answerNoise = .0001
-    answerLivingReward = .8
+    answerDiscount = 0.1
+    answerNoise = 0.0001
+    answerLivingReward = 0.8
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3b():
-    answerDiscount = .1
-    answerNoise = .001
-    answerLivingReward = .8
+    answerDiscount = 0.1
+    answerNoise = 0.001
+    answerLivingReward = 0.8
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3c():
-    answerDiscount = .9
-    answerNoise = .01
-    answerLivingReward = .2
+    answerDiscount = 0.9
+    answerNoise = 0.01
+    answerLivingReward = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question3d():
-    answerDiscount = .9
-    answerNoise = .2
-    answerLivingReward = .2
+    answerDiscount = 0.9
+    answerNoise = 0.2
+    answerLivingReward = 0.2
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
+
 def question3e():
-    answerDiscount = .9
-    answerNoise = .2
+    answerDiscount = 0.9
+    answerNoise = 0.2
     answerLivingReward = 1
     return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
+
 
 def question8():
     answerEpsilon = None
@@ -65,9 +71,10 @@ def question8():
     return answerEpsilon, answerLearningRate
     # If not possible, return 'NOT POSSIBLE'
 
+
 if __name__ == '__main__':
     print('Answers to analysis questions:')
     import analysis
     for q in [q for q in dir(analysis) if q.startswith('question')]:
         response = getattr(analysis, q)()
-        print('  Question %s:\t%s' % (q, str(response)))
+        print(f"  Question {q}:\t{response}")
