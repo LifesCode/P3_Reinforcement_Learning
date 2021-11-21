d2118dca55f9c38878fbc831eff2b79ab159188d
RaffaeleFiorillo <raffadeus8@gmail.com>
2021-11-21 19:44:34 -0100

    Question 7 & 8

--- analysis.py
+++ analysis.py
@@ -66,10 +66,10 @@ def question3e():
 
 
 def question8():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
-    # If not possible, return 'NOT POSSIBLE'
+    answerEpsilon = 0.2
+    answerLearningRate = 0.9
+    # return answerEpsilon, answerLearningRate
+    return 'NOT POSSIBLE'
 
 
 if __name__ == '__main__':
